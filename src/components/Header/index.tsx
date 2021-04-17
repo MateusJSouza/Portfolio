1ba11b185f00f5fa652e003d0cf8f8903a78d336
MateusJesus <mateusjdev@gmail.com>
2021-04-17 14:54:41 -0300

    ✨ teste

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
+import { Container, Content } from "./style";
+
 export function Header() {
-  
+  return (
+    <Container>
+      <Content>
+        <img src="Logo svg" alt="Logo"/>
+
+        <ul className="lista">
+          <li>Sobre mim</li>
+          <li>Habilidades</li>
+          <li>Projetos</li>
+          <li>Contato</li>
+        </ul>
+      </Content>
+    </Container>
+  )
 }
